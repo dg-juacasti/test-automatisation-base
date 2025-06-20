f25f0bc4a444564f2f0f5790a08859320c6e6880
scondolo <scondolo@pichincha.com>
2025-06-20 11:30:51 -0500

    Se agrega pruebas de automatizacion, creacion de escenarios y construccion de karate reports

--- src/test/java/KarateBasicTest.java
+++ src/test/java/KarateBasicTest.java
@@ -4,9 +4,25 @@ class KarateBasicTest {
     static {
         System.setProperty("karate.ssl", "true");
     }
+    
     @Karate.Test
     Karate testBasic() {
         return Karate.run("classpath:karate-test.feature");
     }
+    
+    @Karate.Test
+    Karate testMarvelCharacters() {
+        return Karate.run("classpath:marvelCharactersApi.feature");
+    }
 
+    @Karate.Test
+    Karate testMarvelE2E() {
+        return Karate.run("classpath:marvelE2e.feature");
+    }
+
+    // Método para ejecutar todas las pruebas
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:").tags("~@ignore");
+    }
 }
