5cf4dd6195a1001abd573bc7ea03fbe7fcbba9ea
scondolo <scondolo@pichincha.com>
2025-06-20 11:50:10 -0500

    Se agrega pruebas de automatizacion, creacion de escenarios y construccion de karate reports

---
+++ src/test/resources/karate-config.js
@@ -0,0 +1,17 @@
+function fn() {
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    baseUrl: 'http://bp-se-test-cabcd9b246a5.herokuapp.com',
+    username: 'testuser'
+  }
+  
+  // don't waste time waiting for a connection or if servers don't respond within 5 seconds
+  karate.configure('connectTimeout', 5000);
+  karate.configure('readTimeout', 5000);
+  
+  return config;
+}
