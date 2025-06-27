8491a26a1ea88e1e856e87c84cbc0d1c59ed017a
Kevin Maxi <kmaxi@pichincha.com>
2025-06-27 10:45:50 -0500

    feat: add initial tests

---
+++ src/main/resources/karate-config.js
@@ -0,0 +1,10 @@
+function fn() {
+  const config = {
+    baseUrl: "http://localhost:8080",
+  };
+
+  config.port_marvel_characters_api =
+    "http://localhost:8080/kmaxi/api";
+
+  return config;
+}
